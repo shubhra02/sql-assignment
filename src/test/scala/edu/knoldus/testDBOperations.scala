c49f9632835a5b21990686397f21c7cedb8b1328
shubhra02 <shubhra.sharma1007@gmail.com>
2017-02-11 19:24:09 +0530

    Update testDBOperations.scala

--- src/test/scala/edu/knoldus/testDBOperations.scala
+++ src/test/scala/edu/knoldus/testDBOperations.scala
@@ -5,21 +5,19 @@ import java.sql.Connection
 import org.scalatest.FunSuite
 
 
-class testDBOperations extends FunSuite {
+class DBOperationsTest extends FunSuite {
   val operationObject = new DBOperations
   val connection = new DBConnectivity
-  val user = "root"
-  val password = "root"
+  val eID = 111
+  val pID = 311
+  val cID = 411
+  val dID = 211
   val connectionObject: Connection = connection.getDBConnection
-
-  /**
-    * test case for creating database creation
-    */
 
   test("creation of connection object") {
 
-    assert(connection.getDBConnection.isInstanceOf[Connection] == true
-      || connection.getDBConnection.isInstanceOf[Connection] == false
+    assert(connection.getDBConnection.isInstanceOf[Connection]
+      || !connection.getDBConnection.isInstanceOf[Connection]
     )
   }
 
@@ -29,33 +27,33 @@ class testDBOperations extends FunSuite {
 
   test("throw an exception on wrong query syntax and thus return false on failing of table creation Department") {
     val query = "table Department (deptID Int(5) Primary key NOT NULL, name varchar(20))"
-    assert(operationObject.createTable(connectionObject, query) == false)
+    assert(!operationObject.createTable(connectionObject, query))
 
   }
 
 
   test("to check the table creation Project and it should return true upon table creation Department") {
     val query = "Create table Department (deptID Int(5) Primary key NOT NULL, name varchar(20))"
-    assert(operationObject.createTable(connectionObject, query).isInstanceOf[Boolean] == true)
+    assert(operationObject.createTable(connectionObject, query).isInstanceOf[Boolean])
 
   }
 
   test("to check the table creation Employee and it should return true upon table creation Employee") {
-    val query = "Create table Employee (empID Int(5) Primary key NOT NULL, name varchar(20),"+
+    val query = "Create table Employee (empID Int(5) Primary key NOT NULL, name varchar(20)," +
       s"address varchar(30), phone Int(10), deptId Int(5), projectID Int(5))"
-    assert(operationObject.createTable(connectionObject, query).isInstanceOf[Boolean] == true)
+    assert(operationObject.createTable(connectionObject, query).isInstanceOf[Boolean])
 
   }
 
   test("to check the table creation Department and it should return true upon table creation Project") {
     val query = "Create table Project (projectID Int(5) Primary key NOT NULL, deptID Int(5), name varchar(20), clientID Int(5))"
-    assert(operationObject.createTable(connectionObject, query).isInstanceOf[Boolean] == true)
+    assert(operationObject.createTable(connectionObject, query).isInstanceOf[Boolean])
 
   }
 
   test("to check the table creation Client and it should return true upon table creation Client") {
     val query = "Create table Client (clientID Int(5) Primary key, projectID Int(5), name varchar(20), address varchar(30))"
-    assert(operationObject.createTable(connectionObject, query).isInstanceOf[Boolean] == true)
+    assert(operationObject.createTable(connectionObject, query).isInstanceOf[Boolean])
 
   }
 
@@ -63,24 +61,24 @@ class testDBOperations extends FunSuite {
     * test cases for insertion of data into tables
     */
 
-  test("to check the data insertion into table Department, return on successful insertion else false ") {
-    val insertQuery = Department(12, "science")
-    assert(operationObject.insertToTable(connectionObject, insertQuery).isInstanceOf[Boolean] == true)
+  test("to check the data insertion into table Department, return true on successful insertion else false ") {
+    val insertQuery = Department(211, "science")
+    assert(operationObject.insertToTable(connectionObject, insertQuery).isInstanceOf[Boolean])
   }
 
-  test("to check the data insertion into table Project, return on successful insertion else false") {
-    val insertQuery =  Project(12, 13, "science", 151)
-    assert(operationObject.insertToTable(connectionObject, insertQuery).isInstanceOf[Boolean] == true)
+  test("to check the data insertion into table Project, return true on successful insertion else false") {
+    val insertQuery = Project(311, 13, "science", 151)
+    assert(operationObject.insertToTable(connectionObject, insertQuery).isInstanceOf[Boolean])
   }
 
-  test("to check the data insertion into table Employee, return on successful insertion else false") {
-    val insertQuery =  Employee(12, "Inayat", "Noida", 151, 121, 31)
-    assert(operationObject.insertToTable(connectionObject, insertQuery).isInstanceOf[Boolean] == true)
+  test("to check the data insertion into table Employee, return true on successful insertion else false") {
+    val insertQuery = Employee(111, "Inayat", "Noida", 9825151, 121, 31)
+    assert(operationObject.insertToTable(connectionObject, insertQuery).isInstanceOf[Boolean])
   }
 
-  test("to check the data insertion into table Client, return on successful insertion else false") {
-    val query = "Create table Project (projectID Int(5) Primary key NOT NULL, deptID Int(5), name varchar(20), clientID Int(5),)"
-    assert(operationObject.createTable(connectionObject, query).isInstanceOf[Boolean] == true)
+  test("to check the data insertion into table Client, return true on successful insertion else false") {
+    val insertQuery = Client(411, 311, "aparna", "Pune")
+    assert(operationObject.insertToTable(connectionObject, insertQuery).isInstanceOf[Boolean])
 
   }
 
@@ -107,6 +105,36 @@ class testDBOperations extends FunSuite {
   /**
     * test cases for updation of data in tables
     */
+  test("to check the data updation into table Employee, return true on successful updation else false") {
+    val query = Employee(eID, "Pallavi", "Punjab", 985456151, 121, 31)
+    assert(operationObject.updateTables(connectionObject, query, eID).isInstanceOf[Boolean])
+
+  }
+  test("to check the data updation into table Department, return true on successful updation else false") {
+    val query = Department(dID, "Design")
+    assert(operationObject.updateTables(connectionObject, query, dID).isInstanceOf[Boolean])
+
+  }
+  test("to check the data updation into table Project, return true on successful updation else false") {
+    val query = Project(pID, 122, "Carbon Data", 231)
+    assert(operationObject.updateTables(connectionObject, query, pID).isInstanceOf[Boolean])
+
+  }
+  test("to check the data updation into table Client, return true on successful updation else false") {
+    val query = Client(pID, 121, "aparna", "Kanpur")
+    assert(operationObject.updateTables(connectionObject, query, eID).isInstanceOf[Boolean])
+
+  }
+  /**
+    * test cases for deletion of data from tables
+    */
+
+  test("to check if the data is not successfully deleted from the table"){
+    assert(!operationObject.deleteTable(connectionObject,"Employee", eID))
+  }
+  test("to check if the data is successfully deleted from the tables"){
+    assert(operationObject.deleteTable(connectionObject,"Employee", eID).isInstanceOf[Boolean])
+  }
 
 
 }
